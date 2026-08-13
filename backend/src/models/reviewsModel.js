20886d0fd591bf2dc729c0d32987b076df4793e6
dulanjananisal <dulanjananisal67@gmail.com>
2026-08-14 01:54:25 +0530

    create Reviews Model in reviewsModel.js file

---
+++ backend/src/models/reviewsModel.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const reviewsModel = mongoose.Schema({
+    customer_id: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: [true, 'customer_id is required'],
+        ref: 'CustomerProfile'
+    },
+    seller_id: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: [true, 'seller_id is required'],
+        ref: 'SellerProfile'
+    },
+    comment: {
+        type: String,
+        minlength: [3, 'Comment must have more than 3 letters!']
+    },
+    ratings: {
+        type: Number,
+        required: [true, 'Rating is required!'],
+        maxlength: [5, 'Rating must have less than 5']
+    },
+    images: [
+        {
+            type: String,
+        }
+    ]
+}, { timestamps: true })
+
+module.exports = mongoose.models('Reviews', reviewsModel)
