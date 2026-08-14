f39df152d55d9c41afd7727879cc74f4304730c3
dulanjananisal <dulanjananisal67@gmail.com>
2026-08-14 21:05:55 +0530

    create controllers for register customer and seller

--- backend/src/controllers/authLoginController.js
+++ backend/src/controllers/authLoginController.js
@@ -1,6 +1,8 @@
 const { ACCESS_SECRET, ACCESS_EXPIRED, REFRESH_SECRET, REFRESH_EXPIRED } = require('../config/env');
 const asyncHandler = require('../middleware/asyncHandler');
 const Users = require('../models/userModel');
+const CustomerProfile = require('../models/customerProfileModel');
+const SellerProfile = require('../models/sellerProfileModel');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
@@ -69,7 +71,7 @@ const authLogin = asyncHandler(async (req, res) => {
 		});
 	}
 
-	// create jwt tokens
+	// create jwt tokens and save it into cookie
 	const { accessToken, refreshToken } = createTokenPair(user);
 	setAuthCookies(res, accessToken, refreshToken);
 
@@ -86,14 +88,143 @@ const authLogin = asyncHandler(async (req, res) => {
 
 // Register auth for customers
 const registerCustomers = asyncHandler(async (req, res) => {
-	const { email, password } = req.body;
+	const {
+		email,
+		password,
+		first_name,
+		last_name,
+		phone_number,
+		addresses = [],
+		profile_image,
+		favorite_shops = [],
+		favorite_foods = [],
+	} = req.body;
 
-	if (!email || !password) {
+	// check all required fields are filled
+	if (!email || !password || !first_name || !last_name || !phone_number) {
 		return res.status(400).json({
 			success: false,
-			message: 'Please provide email and password',
+			message: 'Please provide email, password, first name, last name and phone number',
 		});
 	}
+
+	// check user is exist
+	const normalizedEmail = email.trim().toLowerCase();
+	const existingUser = await Users.findOne({ email: normalizedEmail });
+	if (existingUser) {
+		return res.status(400).json({
+			success: false,
+			message: 'Email is already registered!',
+		});
+	}
+
+	// hash password using bcrypt
+	const salt = await bcrypt.genSalt(10)
+	const hashedPassword = await bcrypt.hash(password, salt);
+
+	// create user 
+	const user = await Users.create({
+		email: normalizedEmail,
+		password: hashedPassword,
+		role: 'customer',
+	});
+
+	// create customer profile
+	const customerProfile = await CustomerProfile.create({
+		user_id: user._id,
+		first_name,
+		last_name,
+		addresses,
+		profile_image,
+		favorite_shops,
+		favorite_foods,
+		phone_number,
+	});
+
+	// create jwt token and save it in to cookie
+	const {accessToken, refreshToken} = createTokenPair(user)
+	setAuthCookies(res, accessToken, refreshToken)
+
+	// get response
+	return res.status(201).json({
+		success: true,
+		message: 'Customer registered successfully!',
+		user: {
+			_id: user._id,
+			email: user.email,
+			role: user.role,
+		},
+		customerProfile,
+	});
+});
+
+// Register auth for sellers
+const registerSellers = asyncHandler(async (req, res) => {
+	const {
+		email,
+		password,
+		first_name,
+		last_name,
+		phone_number,
+		profile_image,
+		ratings = 0,
+		rank,
+	} = req.body;
+
+	// check required fields are filled
+	if (!email || !password || !first_name || !last_name || !phone_number) {
+		return res.status(400).json({
+			success: false,
+			message: 'Please provide email, password, first name, last name and phone number',
+		});
+	}
+
+	// check user is already exist
+	const normalizedEmail = email.trim().toLowerCase();
+	const existingUser = await Users.findOne({ email: normalizedEmail });
+	if (existingUser) {
+		return res.status(400).json({
+			success: false,
+			message: 'Email is already registered!',
+		});
+	}
+
+	// hash password
+	const salt = await bcrypt.genSalt(10)
+	const hashedPassword = await bcrypt.hash(password, salt);
+
+	// create user
+	const user = await Users.create({
+		email: normalizedEmail,
+		password: hashedPassword,
+		role: 'seller',
+	});
+
+	// create seller profile
+	const sellerProfile = await SellerProfile.create({
+		user_id: user._id,
+		first_name,
+		last_name,
+		profile_image,
+		phone_number,
+		ratings,
+		rank,
+	});
+
+	// create jwt and save it into cookie
+	const {accessToken, refreshToken} = createTokenPair(user);
+	setAuthCookies(res, accessToken, refreshToken);
+
+	return res.status(201).json({
+		success: true,
+		message: 'Seller registered successfully!',
+		user: {
+			_id: user._id,
+			email: user.email,
+			role: user.role,
+		},
+		sellerProfile,
+	});
 });
 
 // refresh token auth for generate new tokens
@@ -138,4 +269,4 @@ const refreshToken = asyncHandler(async (req, res) => {
 	}
 });
 
-module.exports = { authLogin, refreshToken }; 
+module.exports = { authLogin, refreshToken, registerCustomers, registerSellers };
--- backend/src/routes/authRouter.js
+++ backend/src/routes/authRouter.js
@@ -1,9 +1,11 @@
 const express = require('express');
-const { authLogin, refreshToken } = require('../controllers/authLoginController');
+const { authLogin, refreshToken, registerCustomers, registerSellers } = require('../controllers/authLoginController');
 
 const router = express.Router();
 
 router.post('/login', authLogin);
+router.post('/register', registerCustomers);
+router.post('/register-seller', registerSellers);
 router.post('/refresh-token', refreshToken);
 
 module.exports = router;
