738e9ec070083f4fe9546471f1a658be69e5b2f2
dulanjananisal <dulanjananisal67@gmail.com>
2026-08-18 01:27:52 +0530

    add some functionality for get all customers controller

--- backend/src/controllers/customersController.js
+++ backend/src/controllers/customersController.js
@@ -2,6 +2,16 @@ const asyncHandler = require("../middleware/asyncHandler");
 
 // get all customers with filters
 const getAllCustomers = asyncHandler(async (req,res)=>{
+    const { searchByName } = req.query
+    
+    // create queryData object 
+    const queryData ={}
+
+    // search with name
+    if(searchByName){
+        queryData.first_name = searchByName ? searchByName : '';
+    }
+
     res.status(200).json({success: true, message: 'Get all Customers details'})
 })
 
