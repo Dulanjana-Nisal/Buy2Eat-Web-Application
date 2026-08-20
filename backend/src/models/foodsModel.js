b585a63499bc2bfdf05032233007242008bb5e86
dulanjananisal <dulanjananisal67@gmail.com>
2026-08-20 12:02:25 +0530

    add new foodModel model

---
+++ backend/src/models/foodsModel.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+
+// foodsModel Schema
+const foodsModel = mongoose.Schema({
+    name: {
+        type: String,
+        unique: true,
+        required: [true, 'Food must have a name!']
+    },
+    seller_id: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: [true, 'seller_id is required!']
+    },
+    description: {
+        type: String,
+    },
+    image: {
+        type: String,
+        required: [true, 'Image is required!']
+    },
+    category: {
+        type: String,
+        required: [true, 'food must have category!'],
+        enum: ['rice-and-curry','kottu','hoppers','short-eats','seafood','desserts-and-sweets','beverages','street-food','devilled-dishes','meats','bites']
+    },
+    tags: [{
+        type: String,
+        required: [true, 'tags is required!'],
+    }],
+    availability: {
+        type: Boolean,
+        required: [true, 'availability is required!']
+    },
+    price: {
+        type: Number,
+        required: [true, 'Price is required!']
+    }
+}, { timestamps: true } )
+
+module.exports = mongoose.model('Foods', foodsModel)
