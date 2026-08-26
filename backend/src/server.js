2717cd6dd3828303b21a5e38611c7bacf0ddd945
dulanjananisal <dulanjananisal67@gmail.com>
2026-08-27 02:53:41 +0530

    update cors policy for frontend

--- backend/src/server.js
+++ backend/src/server.js
@@ -14,7 +14,10 @@ const customerRouter = require('./routes/customerRouter');
 const app = express();
 
 // middlewares
-app.use(cors());
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true
+}));
 app.use(express.json());
 app.use(morgan('dev'));
 app.use(cookieParser());
