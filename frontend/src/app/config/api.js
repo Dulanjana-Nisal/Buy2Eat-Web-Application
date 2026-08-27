cd894a1d104f6cd99f23c863b0029d84341d2f91
dulanjananisal <dulanjananisal67@gmail.com>
2026-08-27 13:59:41 +0530

    update env configs

--- frontend/src/app/config/api.js
+++ frontend/src/app/config/api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import env from './env';
+import { env } from "./env";
 
 const api = axios.create({
     baseURL: env.API_BASE_URL,
--- frontend/src/app/config/env.js
+++ frontend/src/app/config/env.js
@@ -1,6 +1,11 @@
-const env = {
-    API_BASE_URL: import.meta.env.VITE_API_URL,
-    CLIENT_URL: import.meta.env.VITE_CLIENT_URL,
+const API_BASE_URL = import.meta.env.VITE_API_URL;
+const CLIENT_URL = import.meta.env.VITE_CLIENT_URL;
+
+if(!API_BASE_URL){
+    throw new Error('VITE_API_URL is required!')
 }
 
-export default env;
+export const env = {
+    API_BASE_URL,
+    CLIENT_URL,
+};
