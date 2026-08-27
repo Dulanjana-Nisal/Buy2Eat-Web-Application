a8cc8da25e51d2c52c0d840979430c50ec17a37e
dulanjananisal <dulanjananisal67@gmail.com>
2026-08-27 14:58:45 +0530

    login page code base moved to separate code files

---
+++ frontend/src/app/App.jsx
@@ -0,0 +1,14 @@
+import LoginPage from '../features/auth/pages/LoginPage';
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
