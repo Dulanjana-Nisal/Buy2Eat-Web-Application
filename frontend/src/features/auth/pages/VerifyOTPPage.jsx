da2ab18111ad3988bb2393d24d8f2ab203cc580a
dulanjananisal <dulanjananisal67@gmail.com>
2026-08-31 03:03:05 +0530

    create functionality for only allow to verification page for register user

--- frontend/src/features/auth/pages/VerifyOTPPage.jsx
+++ frontend/src/features/auth/pages/VerifyOTPPage.jsx
@@ -1,6 +1,22 @@
+import { useEffect } from 'react';
 import Styles from './VerifyOTPPage.module.css';
+import { useNavigate, useLocation } from 'react-router-dom';
 
 function VerifyOTPPage() {
+
+    // navigation state hooks
+    const navigate = useNavigate();
+    const location = useLocation();
+
+    useEffect(() => {
+        // get state data from navigate
+        const verificationId = location.state?.verification_id
+        console.log(verificationId)
+        if(!verificationId){
+            navigate("/register", { replace: true })
+        }
+    }, [])
+
     return(
         <>
             <h1>Verify OTP page</h1>
