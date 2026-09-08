c672240348839c68d171867c8a0c5dcf0f8fdc61
dulanjananisal <dulanjananisal67@gmail.com>
2026-09-08 16:02:28 +0530

    create success page for forgot password

---
+++ frontend/src/features/auth/pages/ForgotPasswordSuccessPage.jsx
@@ -0,0 +1,11 @@
+import styles from './ForgotPasswordSuccessPage.module.css';
+
+function ForgotPasswordSuccessPage(){
+    return (
+        <>
+            <h1 className={styles.container}>Forgot Password Success!</h1>
+        </>
+    )
+}
+
+export default ForgotPasswordSuccessPage;
