c1b048ce59b526b25c12acf6d9c281f8b1816cbd
dulanjananisal <dulanjananisal67@gmail.com>
2026-09-26 02:53:45 +0530

    create authProvider for test it work

---
+++ frontend/src/app/providers/AuthProvider.jsx
@@ -0,0 +1,18 @@
+import { createContext, useContext, useState } from "react";
+
+const AuthContext = createContext();
+
+export default function AuthProvider({ children }){
+
+    const [example,setExample] = useState('some example value')
+
+    return(
+        <AuthContext.Provider value={{ example, setExample }}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
+
+export const UseAuth = () => {
+    return useContext(AuthContext)
+}
