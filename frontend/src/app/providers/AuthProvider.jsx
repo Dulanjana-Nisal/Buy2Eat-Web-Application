5774fc940d5351ead5239e385aa27a3adad24cfc
dulanjananisal <dulanjananisal67@gmail.com>
2026-09-26 13:42:04 +0530

    add auth context to public route to validate user

--- frontend/src/app/providers/AuthProvider.jsx
+++ frontend/src/app/providers/AuthProvider.jsx
@@ -16,7 +16,7 @@ export default function AuthProvider({ children }){
         }
 
         addUserData();
-    }, [])
+    }, []);
 
     return(
         <AuthContext.Provider value={{ user, setUser }}>
--- frontend/src/app/router/PublicRoute.jsx
+++ frontend/src/app/router/PublicRoute.jsx
@@ -1,6 +1,14 @@
-import { Outlet } from "react-router-dom";
+import { Navigate, Outlet } from "react-router-dom";
+import { UseAuth } from "../providers/AuthProvider";
 
 function PublicRoute(){
+    // use UseAuth context for validate route
+    const { user } = UseAuth();
+
+    if (user){
+        return <Navigate to='/' replace />
+    }
+    
     return <Outlet />
 }
 
