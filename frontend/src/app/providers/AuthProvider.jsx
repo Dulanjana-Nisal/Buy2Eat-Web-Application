c30221e95f990a9a48f3b2a265456e8e77070b9b
dulanjananisal <dulanjananisal67@gmail.com>
2026-09-26 13:27:51 +0530

    store localstorage user data to user state in auth context

--- frontend/src/app/providers/AuthProvider.jsx
+++ frontend/src/app/providers/AuthProvider.jsx
@@ -1,11 +1,22 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 
 const AuthContext = createContext();
 
 export default function AuthProvider({ children }){
 
     // use state hook
-    const [user,setUser] = useState(localStorage.getItem('user') || null);
+    const [user,setUser] = useState(null);
+
+    // use useEffect
+    useEffect(() => {
+        const addUserData = () => {
+            // add localstorage user data to user
+            const userData = localStorage.getItem('user') || null
+            setUser(userData ? JSON.parse(userData) : null)
+        }
+
+        addUserData();
+    }, [])
 
     return(
         <AuthContext.Provider value={{ user, setUser }}>
